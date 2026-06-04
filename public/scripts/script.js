60c3f22415fc296ca3e88dc23be80382855aa4d0
Bernardt Dawid van Greunen <148236232+Phasma12-wq@users.noreply.github.com>
2026-06-04 19:32:46 +0200

    Update DOM manipulation and CSS

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -18,53 +18,116 @@ function clearSection() {
 async function displaySkills() {
     clearSection();
 
-    const dict = await getData('skills');
+    const skills = await getData('skills');
+    if (!skills) return; // stop if fetch failed
+
+
+    skills.forEach((sk, index) => {
+        const newDiv = document.createElement("div");
+        newDiv.id = `skill-${index+1}`;
+        newDiv.className = "skill-item";
+
+
+        const h2 = document.createElement("h2");
+        h2.innerText = sk.name;
+
+        const p = document.createElement("p");
+        p.innerText = Array.isArray(sk.skills) ? sk.skills.join(", ") : sk.skills;
+
+        const desc = document.createElement("p");
+        desc.innerText = sk.description || "";
+
+        newDiv.appendChild(h2);
+        newDiv.appendChild(p);
+        newDiv.appendChild(desc);
+
+        container.appendChild(newDiv);
+    })
+
+}
+
+async function displayEducation() {
+    clearSection();
+
+
+    const dict = await getData('education');
     if (!dict) return; // stop if fetch failed
 
 
     let count = 1;
 
     for (const [key, value] of Object.entries(dict)) {
-        
-        /* create div fo skill */
         const newDiv = document.createElement("div");
+        newDiv.id = `edu-${count}`;
+        newDiv.className = "education-item";
+
+
         const h2 = document.createElement("h2");
-        const p = document.createElement("p");
-        
-        newDiv.id = `skill-${count}`;
-        newDiv.className = "skill-item";
+        h2.innerText = value.name;
 
-        h2.innerText = key;
 
-        const skills = Array.isArray(value) ? value.join(', ') : value;
-        p.innerText = skills;
 
+        const ul = document.createElement("ul");
+
+        const shortFormat_from = new Intl.DateTimeFormat('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric'
+        }).format(new Date(value.dateFrom));
+
+        const shortFormat_to = new Intl.DateTimeFormat('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric'
+        }).format(new Date(value.dateTo));
+
+        const li_dates = document.createElement("li")
+        li_dates.innerText = `${shortFormat_from} - ${shortFormat_to}`
+        ul.appendChild(li_dates)
+    
 
         newDiv.appendChild(h2);
-        newDiv.appendChild(p);
+        newDiv.appendChild(ul);
         container.appendChild(newDiv);
 
         count+=1;
     }
 }
 
-/*
-function displayEducation() {
+
+async function displayProjects() {
     clearSection();
 
-    const jsonString = await getData('education')
+    const projects = await getData('projects')
+    if (!projects) return;
 
-    const dict = JSON.parse(jsonString)
+    projects.forEach((proj, index) => {
+        const newDiv = document.createElement("div");
+        newDiv.id = `prj-${index+1}`;
+        newDiv.className = "project-item";
+
+        const h2 = document.createElement("h2");
+        h2.innerText = proj.name || "Untitled Project";
+        
+        const p = document.createElement("p");
+        p.innerText = proj.description || "No description provided";
+
+        const div = document.createElement("div");
+        div.innerText = `Techstack: ${proj.stack.join(', ')}`
+
+        const a = document.createElement("a");
+        a.className = "project_link";
+        a.href = proj.link || "#";
+        a.innerText = "View Project";
+
+        newDiv.appendChild(h2);
+        newDiv.appendChild(p);
+        newDiv.appendChild(div);
+        newDiv.appendChild(a);
+
+        container.appendChild(newDiv);
+    });
 }
-
-function displayProjects() {
-    clearSection();
-
-    const jsonString = await getData('projects')
-
-    const dict = JSON.parse(jsonString)
-}
-*/
 
 async function getData(path) {
   try {
@@ -78,16 +141,11 @@ async function getData(path) {
 }
 
 document.querySelector('.Skills_btn').addEventListener("click", displaySkills);
-/*
 document.querySelector('.Education_btn').addEventListener("click", displayEducation);
 document.querySelector('.Projects_btn').addEventListener("click", displayProjects);
-*/
 
 
 
-
-
-        
 function highlightNav(current_page) {
     const home_link = document.getElementById("home");
     const about_link = document.getElementById("about");
@@ -98,9 +156,9 @@ function highlightNav(current_page) {
         displaySkills();
 
         about_link.style.border = "2px solid black";
-        about_link.style.backgroundColor = "#00f81d"
+        about_link.style.backgroundColor = "#828282";
     } else {
         home_link.style.border = "2px solid black";
-        home_link.style.backgroundColor = "#00f81d"
+        home_link.style.backgroundColor = "#828282";
     }
 }
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -5,45 +5,37 @@ const homeMainInfo = {
     name: "Bernardt Dawid van Greunen",
     degree: "Computing",
     aspiringCareer: "Software Developer",
-    description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    description: "I am a Bachelor of Computing student at Belgium Campus. I am fluent in Afrikaans and " +
+    "English, and proficient in working with computers in general. I'm passionate about building " +
+    " smart, scalable software solutions and exploring the intersection of technology and " +
+    "innovation. With a strong foundation in both frontend and backend development, I enjoy " +
+    "working on diverse projects—from web apps to intelligent drone systems."
 }
 
-const aboutInfo = {
-    Phone: "(+27)65 388 5534",
-    Email: "",
-    profileLinks: {
-        LinkedIn: "",
-        Github: ""
-    },
-    Languages: ["Afrikaans", "English"]
-}
-
-const skills = {
-    Programming: ["C#", "Java", "Object-Oriented Programming (OOP)"],
-    WebProgramming: ["Javascript", "HTML", "CSS", "Node.js", "EJS"],
-    Database: ["SQL", "Database Design"],
-    Other: ["Team Collaboration", "Black-Box testing"]
-}
+const skills = [
+    {name:"Programming", skills: ["C#", "Java", "Object-Oriented Programming (OOP)"], description: "Design software applications according to OOP principles"},
+    {name:"Web Programming", skills: ["Javascript", "HTML", "CSS", "Node.js", "EJS"], description: "Basic reactive websites"},
+    {name:"Database", skills: ["SQL", "Database Design"], description: "Basic Database design and queries"},
+    {name:"Other", skills: ["Team Collaboration"], description: "Collaborate and communicate effectively with other teammates."}
+]
 
 const education = {
-    highSchool: {
+    "High School": {
         name: "H/S Wonderboom South",
-        dateFrom: "01/02/2018",
-        dateTo: "01/12/2022",
-        certificate: ""
+        dateFrom: "02/01/2018",
+        dateTo: "12/01/2022"
     },
-    college: {
-        name: "",
-        dateFrom: "",
-        dateTo: "",
-        certificate: ""
+    College: {
+        name: "Belgium Campus ITversity",
+        dateFrom: "02/01/2023",
+        dateTo: "12/01/2026"
     }
 }
 
 const Projects = [
-    {name: "", description: "", link: ""},
-    {name: "", description: "", link: ""},
-    {name: "", description: "", link: ""}
+    {name: "StreetSync - Community Portal", stack: ["EJS", "Javascript", "CSS", "Express.js"], description: "We utilized what we learned from Web Programming in our 3rd year to develop a website that would run via node.js, in order to implement dynamic rendering via EJS.", link: "https://github.com/Phasma12-wq/WPR381_Group_Assignment.git"},
+    {name: "BC Wellness Web App", stack: ["JavaDB", "Java", "Javascript", "CSS"], description: "We developed a Java desktop application to manage student wellness services at Belgium Campus. It was developed using Java Swing, and JavaDB, and it was designed using OOP principles and MVC architecture. The system provides counselors, students, and administrators with tools to manage appointments, counselor details, and student feedback.", link: "https://github.com/Phasma12-wq/WPR381_Group_Assignment.git"},
+    {name: "Project CampusLearn", stack: ["EJS", "Javascript", "CSS", "Express.js"], description: "For our software engineering assignment, we developed a communication platform for Belgium Campus students, that could be navigated via a gpt chatbot that would perform basic function calling.", link: "https://github.com/DSReaper/Project_CampusLearn.git"}
 ]
 
 router.get('/', (req, res) => {
